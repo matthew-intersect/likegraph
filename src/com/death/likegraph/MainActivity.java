6a3ec4dc3e5e41cc72b078eee6f9b5e2c585f230
matthew-intersect <matthew@intersect.org.au>
2013-10-23 15:52:09 +1100

    Rework facebook login and remove unwanted files from repository

--- src/com/death/likegraph/MainActivity.java
+++ src/com/death/likegraph/MainActivity.java
@@ -1,48 +1,33 @@
 package com.death.likegraph;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.widget.TextView;
-import com.facebook.*;
-import com.facebook.model.*;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity
+{
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+	private MainFragment mainFragment;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+	    super.onCreate(savedInstanceState);
 
-    // start Facebook Login
-    Session.openActiveSession(this, true, new Session.StatusCallback() {
-
-      // callback when session changes state
-      @Override
-      public void call(Session session, SessionState state, Exception exception) {
-        if (session.isOpened()) {
-
-          // make request to the /me API
-          Request.newMeRequest(session, new Request.GraphUserCallback() {
-
-            // callback after Graph API response with user object
-            @Override
-            public void onCompleted(GraphUser user, Response response) {
-              if (user != null) {
-                TextView welcome = (TextView) findViewById(R.id.welcome);
-                welcome.setText("Hello " + user.getName() + "!");
-              }
-            }
-          }).executeAsync();
-        }
-      }
-    });
-  }
-
-  @Override
-  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-      super.onActivityResult(requestCode, resultCode, data);
-      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-  }
-
+	    if (savedInstanceState == null)
+	    {
+	        // Add the fragment on initial activity setup
+	        mainFragment = new MainFragment();
+	        getSupportFragmentManager()
+	        .beginTransaction()
+	        .add(android.R.id.content, mainFragment)
+	        .commit();
+	    }
+	    else
+	    {
+	        // Or set the fragment from restored state info
+	        mainFragment = (MainFragment) getSupportFragmentManager()
+	        .findFragmentById(android.R.id.content);
+	    }
+	}
+	
 }
