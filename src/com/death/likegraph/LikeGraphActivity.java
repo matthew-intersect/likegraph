29148f66e1f643994765911f44453e450cc282de
matthew-intersect <matthew@intersect.org.au>
2013-11-02 14:30:25 +1100

    improve graph by using AChartEngine

--- src/com/death/likegraph/LikeGraphActivity.java
+++ src/com/death/likegraph/LikeGraphActivity.java
@@ -2,11 +2,19 @@ package com.death.likegraph;
 
 import java.util.ArrayList;
 
+import org.achartengine.ChartFactory;
+import org.achartengine.chart.BarChart.Type;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
 import com.jjoe64.graphview.BarGraphView;
 import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.GraphView.GraphViewData;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,20 +46,49 @@ public class LikeGraphActivity extends Activity
 			public void onClick(View v)
 			{
 				ArrayList<Integer> counts = postsDatabaseAdapter.getStatiiCounts();
-				GraphViewData[] graphData = new GraphViewData[counts.size()];
+//				GraphViewData[] graphData = new GraphViewData[counts.size()];
+//				for(int i=0;i<counts.size();i++)
+//				{
+//					graphData[i] = new GraphViewData(i+1, counts.get(i));
+//				}
+//				GraphViewSeries exampleSeries = new GraphViewSeries(graphData);	
+//				BarGraphView graphView = new BarGraphView(v.getContext(), "");
+//				graphView.addSeries(exampleSeries);
+//				graphView.setViewPort(0, 60);
+//				graphView.setScrollable(true);
+//				graphView.setScalable(true);
+//				graphView.getGraphViewStyle().setNumHorizontalLabels(1);
+//				graphView.getGraphViewStyle().setVerticalLabelsWidth(80);
+//				layout.addView(graphView);
+				
+				
+				CategorySeries series = new CategorySeries("");
 				for(int i=0;i<counts.size();i++)
 				{
-					graphData[i] = new GraphViewData(i+1, counts.get(i));
+					series.add("", counts.get(i));
 				}
-				GraphViewSeries exampleSeries = new GraphViewSeries(graphData);	
-				BarGraphView graphView = new BarGraphView(v.getContext(), "");
-				graphView.addSeries(exampleSeries);
-				graphView.setViewPort(0, 60);
-				graphView.setScrollable(true);
-				graphView.setScalable(true);
-				graphView.getGraphViewStyle().setNumHorizontalLabels(1);
-				graphView.getGraphViewStyle().setVerticalLabelsWidth(80);
-				layout.addView(graphView);
+				XYMultipleSeriesDataset dataSet = new XYMultipleSeriesDataset();
+				dataSet.addSeries(series.toXYSeries());
+				XYSeriesRenderer renderer = new XYSeriesRenderer();
+				renderer.setColor(Color.BLUE);
+				XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+				mRenderer.addSeriesRenderer(renderer);
+				mRenderer.setYTitle("Likes");
+				mRenderer.setAxisTitleTextSize(25);
+				mRenderer.setShowGridY(true);
+				mRenderer.setApplyBackgroundColor(true);
+		        mRenderer.setBackgroundColor(Color.BLACK);
+		        mRenderer.setPanEnabled(true, false);
+		        mRenderer.setXAxisMin(-1);
+		        mRenderer.setXAxisMax(60);
+		        mRenderer.setShowLegend(false);
+		        mRenderer.setPanLimits(new double[]{-1,counts.size(),0,series.toXYSeries().getMaxY()});
+		        mRenderer.setXLabels(0);
+		        mRenderer.setLabelsTextSize(30);
+		        mRenderer.setBarSpacing(.1);
+		        mRenderer.setMargins(new int[]{20,50,20,20});
+		        View view = ChartFactory.getBarChartView(getApplicationContext(), dataSet, mRenderer, Type.DEFAULT);
+		        layout.addView(view);
 			}
 		});
 	}
