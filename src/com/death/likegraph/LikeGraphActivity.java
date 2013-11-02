5c5c02f4b58c1d1b95bd7108177735bc4e326b00
matthew-intersect <matthew@intersect.org.au>
2013-11-02 16:52:23 +1100

    UI changes to graph activity + load graph on activity start

--- src/com/death/likegraph/LikeGraphActivity.java
+++ src/com/death/likegraph/LikeGraphActivity.java
@@ -3,29 +3,33 @@ package com.death.likegraph;
 import java.util.ArrayList;
 
 import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
 import org.achartengine.chart.BarChart.Type;
 import org.achartengine.model.CategorySeries;
+import org.achartengine.model.SeriesSelection;
 import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
-
-import com.jjoe64.graphview.BarGraphView;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.GraphView.GraphViewData;
 
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class LikeGraphActivity extends Activity
 {
 	private PostsDatabaseAdapter postsDatabaseAdapter;
 	
-	private Button generate;
+	private XYSeriesRenderer renderer;
+	private XYMultipleSeriesRenderer mRenderer;
+	
 	private LinearLayout layout;
+	private View graph;
+	private CheckBox checkStatii, checkLinks, checkCheckins, checkPhotos;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -36,60 +40,63 @@ public class LikeGraphActivity extends Activity
 	    postsDatabaseAdapter = new PostsDatabaseAdapter(this);
 	    postsDatabaseAdapter = postsDatabaseAdapter.open();
 	    
-	    generate = (Button) findViewById(R.id.generateGraph);
 	    layout = (LinearLayout) findViewById(R.id.sub_layout);
+	    checkStatii = (CheckBox) findViewById(R.id.check_statii);
+	    checkLinks = (CheckBox) findViewById(R.id.check_links);
+	    checkCheckins = (CheckBox) findViewById(R.id.check_checkins);
+	    checkPhotos = (CheckBox) findViewById(R.id.check_photos);
 	    
+	    renderer = new XYSeriesRenderer();
+	    mRenderer = new XYMultipleSeriesRenderer();
 	    
-	    generate.setOnClickListener(new View.OnClickListener()
+	    ArrayList<Integer> counts = postsDatabaseAdapter.getStatiiCounts();
+	    
+	    CategorySeries series = new CategorySeries("");
+		for(int i=0;i<counts.size();i++)
 		{
-			@Override
-			public void onClick(View v)
-			{
-				ArrayList<Integer> counts = postsDatabaseAdapter.getStatiiCounts();
-//				GraphViewData[] graphData = new GraphViewData[counts.size()];
-//				for(int i=0;i<counts.size();i++)
-//				{
-//					graphData[i] = new GraphViewData(i+1, counts.get(i));
-//				}
-//				GraphViewSeries exampleSeries = new GraphViewSeries(graphData);	
-//				BarGraphView graphView = new BarGraphView(v.getContext(), "");
-//				graphView.addSeries(exampleSeries);
-//				graphView.setViewPort(0, 60);
-//				graphView.setScrollable(true);
-//				graphView.setScalable(true);
-//				graphView.getGraphViewStyle().setNumHorizontalLabels(1);
-//				graphView.getGraphViewStyle().setVerticalLabelsWidth(80);
-//				layout.addView(graphView);
-				
-				
-				CategorySeries series = new CategorySeries("");
-				for(int i=0;i<counts.size();i++)
-				{
-					series.add("", counts.get(i));
-				}
-				XYMultipleSeriesDataset dataSet = new XYMultipleSeriesDataset();
-				dataSet.addSeries(series.toXYSeries());
-				XYSeriesRenderer renderer = new XYSeriesRenderer();
-				renderer.setColor(Color.BLUE);
-				XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
-				mRenderer.addSeriesRenderer(renderer);
-				mRenderer.setYTitle("Likes");
-				mRenderer.setAxisTitleTextSize(25);
-				mRenderer.setShowGridY(true);
-				mRenderer.setApplyBackgroundColor(true);
-		        mRenderer.setBackgroundColor(Color.BLACK);
-		        mRenderer.setPanEnabled(true, false);
-		        mRenderer.setXAxisMin(-1);
-		        mRenderer.setXAxisMax(60);
-		        mRenderer.setShowLegend(false);
-		        mRenderer.setPanLimits(new double[]{-1,counts.size(),0,series.toXYSeries().getMaxY()});
-		        mRenderer.setXLabels(0);
-		        mRenderer.setLabelsTextSize(30);
-		        mRenderer.setBarSpacing(.1);
-		        mRenderer.setMargins(new int[]{20,50,20,20});
-		        View view = ChartFactory.getBarChartView(getApplicationContext(), dataSet, mRenderer, Type.DEFAULT);
-		        layout.addView(view);
-			}
-		});
+			series.add("", counts.get(i));
+		}
+		XYMultipleSeriesDataset dataSet = new XYMultipleSeriesDataset();
+		dataSet.addSeries(series.toXYSeries());
+		setupRenderers(counts, series.toXYSeries());
+		mRenderer.setPanLimits(new double[]{-1,counts.size(),0,series.toXYSeries().getMaxY()});
+        graph = ChartFactory.getBarChartView(getApplicationContext(), dataSet, mRenderer, Type.DEFAULT);
+        layout.addView(graph);
+        graph.setOnLongClickListener(new View.OnLongClickListener()
+        {
+        	@Override
+        	public boolean onLongClick(View v)
+        	{
+        		SeriesSelection seriesSelection = ((GraphicalView) graph).getCurrentSeriesAndPoint();
+        		if(seriesSelection != null)
+        		{
+        			Toast.makeText(getApplicationContext(), "Chart element in series index " + seriesSelection.getSeriesIndex()
+        					+ " data point index " + seriesSelection.getPointIndex() + " was clicked"
+        					+ " closest point value X=" + seriesSelection.getXValue() + ", Y=" + seriesSelection.getValue(), Toast.LENGTH_SHORT).show();
+        		}
+        		return false;
+        	}
+        });
+	}
+	
+	private void setupRenderers(ArrayList<Integer> counts, XYSeries series)
+	{
+		renderer.setColor(Color.BLUE);
+		mRenderer.addSeriesRenderer(renderer);
+		mRenderer.setYTitle("Likes");
+		mRenderer.setAxisTitleTextSize(25);
+		mRenderer.setShowGridY(true);
+		mRenderer.setApplyBackgroundColor(true);
+        mRenderer.setBackgroundColor(Color.BLACK);
+        mRenderer.setPanEnabled(true, false);
+        mRenderer.setPanLimits(new double[]{-1, counts.size(), 0, series.getMaxY()});
+        mRenderer.setXAxisMin(-1);
+        mRenderer.setXAxisMax(50);
+        mRenderer.setShowLegend(false);
+        mRenderer.setXLabels(0);
+        mRenderer.setLabelsTextSize(30);
+        mRenderer.setBarSpacing(.1);
+        mRenderer.setMargins(new int[]{20, 50, 0, 20});
+        mRenderer.setClickEnabled(true);
 	}
 }
